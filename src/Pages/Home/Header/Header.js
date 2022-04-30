ed55ec559022e8d59c47c701a8d562effcf02c86
SoubirSaian <soubir2018@gmail.com>
2022-04-30 18:23:42 +0600

    header-section

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Header.css';
+import  Typography  from '@mui/material/Typography';
+
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <Typography variant='h2' component='div' sx={{pt:'200px',pl:'300px',color:'#fff'}}>
+                    Cari Cari
+            </Typography>
+            <Typography variant='body1' component='div' sx={{color: '#fff',pt:1,pl:'300px'}}>
+                    Live from their sofa to yours. Get closer to your favourite
+                    
+            </Typography>
+            <Typography variant='body1' component='div' sx={{color: '#fff',pl:'300px'}}>
+                artists and never miss out.
+                    
+            </Typography>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header/Header';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='homepage'>
+            <Header></Header>
         </div>
     );
 };
